8f5e2fa6d6acf406edcb56611308dc621c394261
Lev Zakharov <l.j.zakharov@gmail.com>
2018-02-07 15:32:50 +0300

    Add API root page

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,30 @@
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.decorators import api_view
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.renderers import StaticHTMLRenderer
 from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import UserSerializer, SnippetSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class GenericUserView(GenericAPIView):
