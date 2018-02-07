2743263233d785f9317f5baf46dbd30ba3365c72
Lev Zakharov <l.j.zakharov@gmail.com>
2018-02-07 14:35:35 +0300

    Add serializer for 'User' model

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+from snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
