27d16a7d0ffc2ddaf4cbffe391ee292451bdfaa1
Lev Zakharov <l.j.zakharov@gmail.com>
2018-02-07 14:20:42 +0300

    Refactor views using mixed-in generic views

--- snippets/views.py
+++ snippets/views.py
@@ -1,39 +1,20 @@
-from rest_framework import mixins
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class GenericSnippetView(GenericAPIView):
+class GenericSnippetView():
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  GenericSnippetView):
+class SnippetList(GenericSnippetView, ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    GenericSnippetView):
+class SnippetDetail(GenericSnippetView, RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
